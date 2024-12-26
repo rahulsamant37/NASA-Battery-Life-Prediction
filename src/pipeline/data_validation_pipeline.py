aacc05ba925c95b9a86870368a272d4b193533f2
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-12-26 23:50:12 +0530

    setting-up src/pipeline for data validation

---
+++ src/pipeline/data_validation_pipeline.py
@@ -0,0 +1,18 @@
+from src.config.configuration import ConfigurationManager
+from src.components.data_validation import (DataValidation)
+from src import logger
+
+STAGE_NAME="Data Validation Stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_validation(self):
+        try:
+            config = ConfigurationManager()
+            data_validation_config = config.get_data_validation_config()
+            data_validation = DataValidation(config=data_validation_config)
+            data_validation.validate_all_columns()
+        except Exception as e:
+            raise e
