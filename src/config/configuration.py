7874ff4ea7c02343ff9722f8e0bb1d3a1ebd6fab
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-12-27 07:38:00 +0530

    setting-up src/config for model training

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,6 +1,6 @@
 from src.constants import *
 from src.utils.common import read_yaml, create_directories
-from src.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig)
+from src.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -50,3 +50,26 @@ class ConfigurationManager:
             data_path=config.data_path
         )
         return data_tranformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config=self.config.model_trainer
+        params=self.params.model_trainer
+        schema=self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path=config.train_data_path,
+            test_data_path=config.test_data_path,
+            model_name=config.model_name,
+            epochs=params.epochs,
+            batch_size=params.batch_size,
+            optimizer=params.optimizer,
+            loss=params.loss,
+            validation_split=params.validation_split,
+            gru_units=params.gru_units,
+            dropout_rate=params.dropout_rate,
+            target_column=schema.name
+        )
+        return model_trainer_config
