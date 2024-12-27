2b1751199300056c1003c9af7f5b6831c25ce3a2
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-12-27 07:37:24 +0530

    setting-up src/entity for model training

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -22,3 +22,18 @@ class DataTransformationConfig:
     seq_length: int = 2
     test_size: float = 0.25
     random_state: int = 42
+
+@dataclass
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    epochs: int
+    batch_size: int
+    optimizer: str
+    loss: str
+    validation_split: float
+    gru_units: int
+    dropout_rate: float
+    target_column: str
