6c40b78dec74db1fad7ad4bdb77651127ece4583
rahulsamant37 <rahulsamantcoc2@gmail.com>
2024-12-27 07:39:31 +0530

    Initializing main.py for model training

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src import logger
 from src.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.pipeline.model_training_pipeline import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -28,6 +29,16 @@ try:
       logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
       data_transformation = DataTransformationTrainingPipeline()
       data_transformation.inititate_data_transformation()
+      logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+      logger.exception(e)
+      raise e
+
+STAGE_NAME = "Model Training stage"
+try:
+      logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+      data_transformation = ModelTrainingPipeline()
+      data_transformation.inititate_model_training()
       logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
       logger.exception(e)
